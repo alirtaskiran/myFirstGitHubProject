8610af1dfdb84beccc499c187e4ae0c77367e005
Ali Rıza TAŞKIRAN <46461118+alirtaskiran@users.noreply.github.com>
2022-07-17 23:51:59 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-            System.out.println("Hello GitHub"); // bunu bir de method yardımı ile yapacağız.
+            System.out.println("Hello GitHub"); // buna benzer bir selamlamayı method yardımıylada yapacağız.
 
     }
 }
